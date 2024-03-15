8b6cd840888b1761ab32b35da797f3978f30e50d
ChainCoderPro <chaincoderpro@gmail.com>
2024-03-15 18:58:28 +1000

    update rodeo apy

--- src/api/stats/arbitrum/getRodeoApys.js
+++ src/api/stats/arbitrum/getRodeoApys.js
@@ -1,5 +1,6 @@
 const BigNumber = require('bignumber.js');
 
+const RodeoPoolAbi = require('../../../abis/RodeoPool.json');
 const LiquidityMiningAbi = require('../../../abis/RodeoLiquidityMining.json');
 const pools = require('../../../data/arbitrum/rodeoPools.json');
 const { default: ERC20Abi } = require('../../../abis/ERC20Abi');
@@ -7,6 +8,7 @@ import { fetchContract } from '../../rpc/client';
 import getApyBreakdown from '../common/getApyBreakdown';
 
 const liquidityMiningAddr = '0x3aEe6cA602C060883201B89c64cb5F782F964879';
+const rodeoPoolAddr = '0x0032f5e1520a66c6e572e96a11fbf54aea26f9be';
 const RDO = {
   address: '0x033f193b3Fceb22a440e89A2867E8FEE181594D9',
 };
@@ -17,7 +19,7 @@ const getRodeoApys = async () => {
   pools.forEach(pool => promises.push(getPoolApy(pool)));
   const farmAprs = await Promise.all(promises);
 
-  const res =  getApyBreakdown(pools, {}, farmAprs);
+  const res = getApyBreakdown(pools, {}, farmAprs);
   console.log(res)
 
   return res;
@@ -80,13 +82,23 @@ const getYearlyRewardsInUsd = async (poolId, chainId) => {
 };
 
 const getPoolApy = async pool => {
+  const rodeoPoolContract = fetchContract(rodeoPoolAddr, RodeoPoolAbi, pool.chainId)
+  const borrowedAmount = new BigNumber((await rodeoPoolContract.read.getTotalBorrow()).toString())
+  const lentAmount = new BigNumber((await rodeoPoolContract.read.getTotalLiquidity()).toString())
+  const utilization = Number(borrowedAmount.dividedBy(lentAmount).toString()) * 1e18
+  const kink = 850000000000000000, high = 38051750380, low = 951293759
+  let rate = 317097919
+
+  if (utilization > kink) rate += (low / 1e18 * kink) + ((utilization - kink) / 1e18 * high)
+  else rate += (low / 1e18 * utilization)
+
   const [yearlyRewardsInUsd, totalStakedInUsd] = await Promise.all([
     getYearlyRewardsInUsd(pool.poolId, pool.chainId),
     getTotalStakedInUsd(pool.address, pool.oracle, pool.oracleId, pool.decimals, pool.chainId),
   ]);
 
-  console.log(yearlyRewardsInUsd.toString(), totalStakedInUsd.toString())
-  return yearlyRewardsInUsd.dividedBy(totalStakedInUsd);
+  console.log("*******************************************", rate, yearlyRewardsInUsd.dividedBy(totalStakedInUsd).toString(), lentAmount.toString(), borrowedAmount.toString(), utilization, yearlyRewardsInUsd.dividedBy(totalStakedInUsd).plus(rate / 1e10).toString())
+  return yearlyRewardsInUsd.dividedBy(totalStakedInUsd).plus(rate / 1e10);
 };
 
 module.exports = getRodeoApys;
